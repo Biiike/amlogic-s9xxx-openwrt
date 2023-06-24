cc524d4e8d7565280065cadc14a003f27e263193
Biiike <97334429+Biiike@users.noreply.github.com>
2023-06-24 23:48:13 +0800

    Update diy-part2.sh

--- config-openwrt/lede-master/diy-part2.sh
+++ config-openwrt/lede-master/diy-part2.sh
@@ -31,6 +31,10 @@ echo "DISTRIB_SOURCECODE='lede'" >>package/base-files/files/etc/openwrt_release
 # Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
+rm -rf package/lean/openwrt-packages
+
+git clone https://github.com/haiibo/openwrt-packages package/lean/openwrt-packages
+
 # Fix runc version error
 # rm -rf ./feeds/packages/utils/runc/Makefile
 # svn export https://github.com/openwrt/packages/trunk/utils/runc/Makefile ./feeds/packages/utils/runc/Makefile
